8125ea8ee0ff2cfbc2703e8e23726656805adbbb
mrsoc2509 <mrsoc2509@gmail.com>
2021-04-09 23:59:59 +0700

    Tiri up

--- AAA/BackendApi/Controllers/HomeController.cs
+++ AAA/BackendApi/Controllers/HomeController.cs
@@ -1,11 +1,11 @@
-﻿using BackendApi.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BackendApi.Models;
 
 namespace BackendApi.Controllers
 {
@@ -20,18 +20,8 @@ namespace BackendApi.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return Ok();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
