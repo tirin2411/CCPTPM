7676f2dc7ad04af82ccfde922d470704874c6edc
mrsoc2509 <mrsoc2509@gmail.com>
2021-04-09 17:38:19 +0700

    check

--- AAA/AdminApp/Controllers/HomeController.cs
+++ AAA/AdminApp/Controllers/HomeController.cs
@@ -1,15 +1,16 @@
-﻿using AdminApp.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AdminApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AdminApp.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -20,10 +21,16 @@ namespace AdminApp.Controllers
 
         public IActionResult Index()
         {
+            var user = User.Identity.Name;
             return View();
         }
 
         public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
         {
             return View();
         }
--- AAA/AdminApp/Startup.cs
+++ AAA/AdminApp/Startup.cs
@@ -1,12 +1,16 @@
+using System;
+using System.IO;
+using ApiSer;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ViewModels.System.Users;
 
 namespace AdminApp
 {
@@ -22,7 +26,50 @@ namespace AdminApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddHttpClient();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Login/Index";
+                    options.AccessDeniedPath = "/User/Forbidden/";
+                });
+
+            services.AddControllersWithViews()
+                .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<LoginRequestValidator>());
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+            });
+
+            //Declare DI
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddTransient<IUserApiClient, UserApiClient>();
+            services.AddTransient<IMangaApiClient, MangaApiClient>();
+            services.AddTransient<IRoleApiClient, RoleApiClient>();
+            services.AddTransient<IOrderApiClient, OrderApiClient>();
+            services.AddTransient<ITheloaiApiClient, TheloaiApiClient>();
+            services.AddTransient<IMenuApiClient, MenuApiClient>();
+            services.AddTransient<ITintucApiClient, TintucApiClient>();
+            services.AddTransient<IBannerApiClient, BannerApiClient>();
+            services.AddTransient<IKhuyenmaiApiClient, KhuyenmaiApiClient>();
+            services.AddTransient<IStatisticApiClient, StatisticApiClient>();
+            services.AddTransient<IPriceAndDestinationApiClient, PriceAndDestinationApiClient>();
+            services.AddTransient<ILocalApiClient, LocalApiClient>();
+            
+
+            IMvcBuilder builder = services.AddRazorPages();
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+#if DEBUG
+            if (environment == Environments.Development)
+            {
+                builder.AddRazorRuntimeCompilation();
+            }
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,13 +82,28 @@ namespace AdminApp
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
+            app.UseHttpsRedirection();
+            // For wwwroot directory
             app.UseStaticFiles();
+            //// Add support for node_modules but only during development **temporary**
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseStaticFiles(new StaticFileOptions()
+            //    {
+            //        FileProvider = new PhysicalFileProvider(
+            //          Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
+            //        RequestPath = new PathString("wwwroot/lib")
+            //    });
+            //}
+            app.UseAuthentication();
 
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseSession();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
